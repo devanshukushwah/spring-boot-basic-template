d4d166e3654e76031c3f21f8a45ec8e4d30c1a56
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2023-01-21 17:13:12 +0530

    setting up git

---
+++ src/main/java/com/dev/app/MyDevAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/app/MyDevAppApplicationTests.java
@@ -0,0 +1,7 @@
+package com.dev.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevAppApplicationTests {
+}
